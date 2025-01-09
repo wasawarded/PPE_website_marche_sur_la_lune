aeabd2e92a9591d9e1918bd85a419150d9565634
﷽ <ghammatt@hotmail.com>
2025-01-10 00:14:50 +0100

    mise à jour travail Ihsane

--- programes/make_pals_corpus_ar_fr.sh
+++ programes/make_pals_corpus_ar_fr.sh
@@ -6,7 +6,7 @@ AR_CONTEXT_DIR="$BASE_DIR/contextes/ar_contexte/context_txt"
 FR_CONTEXT_DIR="$BASE_DIR/contextes/fr_contexte/context_txt"
 AR_DUMPS_DIR="$BASE_DIR/dumps-text/logique_arabe"
 FR_DUMPS_DIR="$BASE_DIR/dumps-text/logique_français"
-PALS_DIR="$BASE_DIR/dumps-text/pals"
+PALS_DIR="$BASE_DIR/pals/dumps-text"
 
 # Arguments pour le script
 input_folder=$1       # Dossier contenant les fichiers à traiter
@@ -20,8 +20,13 @@ process_file() {
     input_file=$1
     output_file=$2
 
-    # Tokenisation : un mot par ligne, suppression des lignes vides
-    cat "$input_file" | tr -s '[:space:]' '\n' | sed '/^$/d' > "$output_file"
+    # Tokenisation : un mot par ligne, maintien des lignes vides entre phrases
+    awk '{
+        for (i=1; i<=NF; i++) {
+            print $i
+        }
+        print ""  # Ajout d'une ligne vide entre chaque ligne d'entrée
+    }' "$input_file" | sed '/^$/N;/^\n$/D' > "$output_file"
 }
 
 # Vérification et assignation du bon dossier d'entrée
